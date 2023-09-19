9d88952e1a5c36750c8bc4c654efef065b353298
hxj5 <hxj5@hku.hk>
2023-09-19 10:10:55 +0800

    fix commit conflict

--- R/matrix.R
+++ R/matrix.R
@@ -3,19 +3,12 @@
 
 #' DataFrame and Matrix Conversion
 #'
-<<<<<<< HEAD
 #' The conversion between long-format dataframe and (sparse) matrix.
 #'
 #' @param df A long-format dataframe. It should contain at least 3 columns 
 #'   specified by params `row`, `col`, and `value`.
 #' @param mtx A matrix. It should be regular matrix for `mtx_mtx2df()` and
 #'   sparse matrix of `dgCMatrix` class for `mtx_sparse_mtx2df()`.
-=======
-#' The conversion between dataframe and (sparse) matrix.
-#'
-#' @param df A dataframe. It should contain at least 3 columns specified by
-#'   params `row`, `col`, and `value`.
->>>>>>> 42582cea2f43dac457911e8857992e2174bb4d6c
 #' @param row A string. The name of a column in `df` that contains the row
 #'   names of the matrix.
 #' @param col A string. The name of a column in `df` that contains the column
@@ -24,18 +17,12 @@
 #'   values of the matrix.
 #' @param na.values The value to replace NAs introduced during conversion.
 #'   Setting to `NA` or `NULL` to skip the value replacement.
-<<<<<<< HEAD
 #' @param full A bool. Whether to return all combinations of rows and columns.
 #' @return
 #' * The `mtx_df2mtx()` returns a matrix.
 #' * The `mtx_df2sparse_mtx()` returns a sparse matrix of `dgCMatrix` class.
 #' * The `mtx_mtx2df()` returns a long-format dataframe.
 #' * The `mtx_sparse_mtx2df()` returns a long-format dataframe.
-=======
-#' @return
-#' * The `mtx_df2mtx` returns a matrix.
-#' * The `mtx_df2sparse_mtx` returns a sparse matrix of `dgCMatrix` type.
->>>>>>> 42582cea2f43dac457911e8857992e2174bb4d6c
 #'
 #' @section Notes:
 #' The param `na.values` could affect the `NA`s that already exist in the
@@ -49,15 +36,10 @@
 #'   v = 1:3,
 #'   stringsAsFactors = FALSE
 #' )
-<<<<<<< HEAD
 #' mtx1 <- mtx_df2mtx(df, row = "x", col = "y", value = "v")
 #' mtx2 <- mtx_df2sparse_mtx(df, row = "x", col = "y", value = "v")
 #' df1 <- mtx_mtx2df(mtx1, row = "x", col = "y", value = "v")
 #' df2 <- mtx_sparse_mtx2df(mtx2, row = "x", col = "y", value = "v")
-=======
-#' mtx <- mtx_df2mtx(df, row = "x", col = "y", value = "v")
-#' mtx2 <- mtx_df2sparse_mtx(df, row = "x", col = "y", value = "v")
->>>>>>> 42582cea2f43dac457911e8857992e2174bb4d6c
 #' @name df-mtx-convert
 NULL
 
@@ -78,11 +60,7 @@ mtx_df2mtx <- function(df, row, col, value, na.values = 0) {
   df_tran <- df %>%
     tidyr::spread(key = col, value = value)
 
-<<<<<<< HEAD
   row_names <- df_tran[, 1]  
-=======
-  row_names <- df_tran[, 1]    # CHECK ME! what if duplicate column names?
->>>>>>> 42582cea2f43dac457911e8857992e2174bb4d6c
   df_tran <- df_tran[, -1]
 
   if (! is.na(na.values) && ! is.null(na.values))
@@ -99,7 +77,6 @@ mtx_df2mtx <- function(df, row, col, value, na.values = 0) {
 mtx_df2sparse_mtx <- function(df, row, col, value, na.values = 0) {
   mtx <- mtx_df2mtx(df, row = row, col = col, value = value, 
                     na.values = na.values)
-<<<<<<< HEAD
   mtx_s <- methods::as(mtx, "dgCMatrix")
   return(mtx_s)
 }
@@ -140,10 +117,6 @@ mtx_sparse_mtx2df <- function(mtx, row, col, value, full = FALSE) {
     colnames(df) <- c(row, col, value)
     return(df)
   }
-=======
-  mtx_s <- Matrix::Matrix(mtx, sparse = TRUE)
-  return(mtx_s)
->>>>>>> 42582cea2f43dac457911e8857992e2174bb4d6c
 }
 
 
